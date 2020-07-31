036a3645c5abcb7a0a26099b801f435bbdc20440
Furkan Bayram <hello@sfb.dev>
2020-08-01 01:09:33 +0300

    İlk kata eklendi.

---
+++ kata1.js
@@ -0,0 +1,10 @@
+﻿let returnObject = { even: [], odd: [] }, numbers = [1, 2, 3, 4, 5]
+
+const splitOddAndEven = numbers => {
+  numbers.filter((item) => {
+    item % 2 == 0 ? returnObject.even.push(item) : returnObject.odd.push(item)
+  })
+  return returnObject
+}
+
+splitOddAndEven(numbers)
