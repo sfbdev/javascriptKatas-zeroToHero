3c7cadc03e0c88d5196da5ec02d691a7aa66b6f8
Furkan Bayram <hello@sfb.dev>
2020-08-01 01:25:21 +0300

    İkinci kata eklendi.

---
+++ kata2.js
@@ -0,0 +1,11 @@
+﻿const inputString = 'furkan bayram', returnObject = {}
+
+const countAmountOfEachCharacter = inputString => {
+  inputString = inputString.split("")
+  inputString.forEach((item) => {
+    returnObject.hasOwnProperty(item) ? returnObject[item] += 1 : returnObject[item] = 1;
+  })
+  return returnObject
+}
+
+countAmountOfEachCharacter(inputString)
